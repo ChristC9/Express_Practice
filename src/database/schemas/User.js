82771ba860c9b3daddeddc5cac041b54002c290b
ChristC9 <nyanhtetaungucsy106@gmail.com>
2023-11-22 00:13:47 +0630

    ExpressJS - Basic Username & Password Authentication

--- src/database/schemas/User.js
+++ src/database/schemas/User.js
@@ -1,14 +1,12 @@
 const { Schema, model } = require('mongoose');
 const UserSchema = new Schema({
-    username: {
+
+    email: {
         type: String,
         required: true,
+        unique: true,
     },
     password: {
-        type: String,
-        required: true
-    },
-    email: {
         type: String,
         required: true
     },
--- src/routes/login.js
+++ src/routes/login.js
@@ -2,33 +2,33 @@ const { Router } = require("express");
 
 const router = Router();
 const User = require('../database/schemas/User')
+const { hashPassword, comparePassword } = require('../utils/helpers')
+router.post('/api/login', async (req, res) => {
 
-router.post('/api/login', (req, res) => {
-
-    const { username, password } = req.body;
-    console.log(username, password)
-    if (username && password) {
-        if (req.session.user) {
-            res.send(req.session.user)
-        } else {
-            req.session.user = {
-                username
-            }
-            res.send(req.session.user)
-        }
-    } else {
+    const { email, password } = req.body;
+    if (!email || !password) res.send(400)
+    const userDB = await User.findOne({ email })
+    if (!userDB) {
         res.send(401)
+    } else {
+        const isValid = comparePassword(password, userDB.password)
+        if (isValid) {
+            req.session.user = userDB
+            return res.send(200, 'User Logged In')
+        } else {
+            return res.send(500, 'Invalid Password')
+        }
     }
-
 })
 
 router.post('/api/user/register', async (req, res) => {
-    const { username, password, email } = req.body;
-    const userDB = await User.findOne({ $or: [{ username }, { email }] })
+    const { email } = req.body;
+    const userDB = await User.findOne({ email })
     if (userDB) {
         res.send(401, 'User Already Exists')
     } else {
-        const newUser = new User({ username, password, email })
+        const password = hashPassword(req.body.password)
+        const newUser = new User({ email, password })
         await newUser.save()
         res.send('User Created')
     }
